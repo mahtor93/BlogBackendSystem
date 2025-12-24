f241cbf03860656771735fc5230520549ddd2109
Mario H. Tornería Ramírez <mario.torneria@virginiogomez.cl>
2025-12-24 12:18:25 -0300

    Implementación de tags, terminando ciclo de creación de posts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,3 +1,4 @@
+import e from "express";
 import prisma from "../config/database.js";
 
 export async function createPost(req, res){
@@ -20,3 +21,76 @@ export async function createPost(req, res){
     }
 }
 
+export async function addPostMedia(req, res) {
+    try{
+        const postId = parseInt(req.params.id);
+        const userId = req.user.id;
+        const { mediaUrl } = req.body;
+
+        if(!mediaUrl){
+            return res.status(400).json({ message: "Media URL is required" });
+        }
+
+        const post = await prisma.post.findUnique({
+            where: { id: postId },
+            select: { authorId: true }
+        });
+
+        if(!post || post.authorId !== userId){
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+
+        const headerCount = mediaUrl.filter(m=> m.isHeader).length;
+        if(headerCount > 1){
+            return res.status(400).json({ message: "Only one header media allowed" });
+        }
+
+        const newMedia = await prisma.mediaUrl.createMany({
+            data: mediaUrl.map(m=>({
+                postId,
+                type:m.type,
+                url: m.url,
+                position: m.position ?? null,
+                isHeader: m.isHeader ?? false
+            }))
+        });
+
+        res.status(201).json({ message: "Media added successfully" });
+
+    }catch (error) {
+        console.error("Add Post Media Error:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+export async function addPostTags(req, res) {
+    try{
+        const postId = req.params.id;
+        const userId = req.user.id;
+        const { tags } = req.body;
+
+        if(!Array.isArray(tags) || tags.length === 0){
+            return res.status(400).json({ message: "Tags are required" });
+        }
+
+        const normalizedTags = [...new Set(
+            tags.map(t=> t.trim().toLowerCase())
+        )];
+
+        const post = await prisma.post.findUnique({
+            where: { id: postId },
+            select: { authorId: true }
+        });
+
+        if(!post || post.authorId !== userId){
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        const createdTags= [];
+
+    }catch (error) {
+        console.error("Add Post Tags Error:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { createPost } from "../controllers/posts.controller.js";
+import { createPost, addPostMedia, addPostTags } from "../controllers/posts.controller.js";
+import authenticateToken from "../middlewares/auth.js";
 
 const postRouter = Router();
+const postPublicRouter = Router();
+const postPrivateRouter = Router();
 
-postRouter.post("/post", createPost);
-postRouter.get("/post/:id");
-postRouter.patch("/post/:id");
-postRouter.delete("/post/:id");
+postPrivateRouter.use(authenticateToken);
 
+/* RUTAS PUBLICAS */
+
+
+/* RUTAS PRIVADAS */
+postPrivateRouter.post("/posts", createPost);
+postPrivateRouter.post("/posts/:id/media", addPostMedia);
+postPrivateRouter.post("/post/:id/tags", addPostTags);
+
+/* UNIFICACIÓN DE RUTAS */
+postRouter.use("/", postPublicRouter);
+postRouter.use("/", postPrivateRouter);
 export default postRouter;
